7dc9bcd9d5d0c0ca89e48c49aa17e4b0266a249a
sachinpoudel <poudelsachin009@gmail.com>
2025-10-21 19:54:46 +0545

    deployed url pasted in frontend

--- frontend/src/ws.ts
+++ frontend/src/ws.ts
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 
 export const connectWS = () => {
-    const socket = io("http://localhost:3000");
+    const socket = io(import.meta.env.VITE_WS_URL || "http://localhost:3000");
     return socket;
 };
